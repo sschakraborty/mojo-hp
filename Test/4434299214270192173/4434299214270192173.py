ecef60a93e520fd5a4b2134d3601202feb7a5c79
root <subhadrasundar@gmail.com>
2018-06-23 11:04:02 +0530

    UPGRADE: Vert.x 3.5.0 -> Vert.x 3.5.2

---
+++ Test/4434299214270192173/4434299214270192173.py
@@ -0,0 +1,18 @@
+def expo(a, b, m):
+	p = 1
+	while b > 1:
+		if (b & 1) == 0:
+			a *= a
+			a %= m
+			b >>= 1
+		else:
+			p *= a
+			p %= m
+			b -= 2
+	return p
+
+if __name__ == "__main__":
+	MOD = 10 ** 7 + 9
+	for i in range(3):
+		x = input().split()
+		print(expo(int(x[0]), int(x[1]), MOD))
